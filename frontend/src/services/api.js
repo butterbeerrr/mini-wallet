139dfe8b420c2c6a75648745036ae7dacd584f6f
butterbeerrr <ranisa@student.telkomuniversity.ac.id>
2026-09-10 15:39:39 +0800

    complete mini wallet

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+    },
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+    const token = localStorage.getItem("token");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
 
-  return config;
+    return config;
 });
 
 export default api;
